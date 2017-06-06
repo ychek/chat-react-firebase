12897d9cc997bfe846fe0bb30800940cf751ac6f
Yaniv Chekroun <ychek5@gmail.com>
2017-06-06 10:42:24 +0300

    Project scaffolding and username handling

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import * as firebase from 'firebase'
+
+
+let config = {
+    apiKey: "AIzaSyAq6GLAZRaxTamJP7PWycWp3akQj0AUkGQ",
+    authDomain: "chat-4bbc0.firebaseapp.com",
+    databaseURL: "https://chat-4bbc0.firebaseio.com",
+    projectId: "chat-4bbc0",
+    storageBucket: "chat-4bbc0.appspot.com",
+    messagingSenderId: "10795783710"
+};
+
+// Initialize Firebase
+firebase.initializeApp(config);
+
+
+function getUserName() {
+    let person = prompt("Please choose a username", "Username");
+    if (person) {
+       return person
+    }
+    else {
+        let randomNumber = getRandomInt(0, 1000);
+        return "user" + randomNumber
+    }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+let person = getUserName();
+
+
+ReactDOM.render(<App username={person} />, document.getElementById('root'));
+registerServiceWorker();
