d839217b44d1de16faae25f022a2789d352c82be
Yaniv Chekroun <ychek5@gmail.com>
2017-06-07 12:01:25 +0300

    remove un-necessary id on the message obj

--- src/App.js
+++ src/App.js
@@ -15,12 +15,7 @@ class App extends Component {
 
         this.state = {
             userMessage: '',
-            messages: [
-                // {name: 'user1', msg: 'Hello', timestamp: Date.now() },
-                // {name: 'user2', msg: 'Helloooo', timestamp: Date.now()},
-                // {name: 'user3', msg: 'Hello text 3', timestamp: Date.now()},
-                // {name: 'user4', msg: 'Hello text4', timestamp: Date.now()},
-            ],
+            messages: [],
         }
 
     }
@@ -34,14 +29,13 @@ class App extends Component {
     postUserMsg(event) {
         const username = this.props.username;
         const newMsgToBePosted = {
-            id: this.state.messages.length,
             name: username,
             msg: this.state.userMessage,
             timestamp: Date.now()
         };
 
         if (newMsgToBePosted.msg) { //prevent sending empty messages
-            firebase.database().ref('messages/' + newMsgToBePosted.id).set(newMsgToBePosted);
+            firebase.database().ref('messages/' + this.state.messages.length).set(newMsgToBePosted);
         }
 
 
@@ -53,7 +47,7 @@ class App extends Component {
         firebase.database().ref('messages/').on('value', snap => {
 
             const dbMessages = snap.val();
-
+            console.log(dbMessages);
             if (dbMessages){
                 this.setState({
                     messages: dbMessages
